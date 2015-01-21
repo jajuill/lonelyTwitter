5068e09d52003076a502943cafa7def19a0b2432
Matthew Mcleod <mamcleod@cs.ualberta.ca>
2015-01-21 15:03:37 -0700

    added file???

--- src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,9 +1,26 @@
 package ca.ualberta.cs.lonelytwitter;
 
+/*
+onCreate, onStart, on Resume  (all must be called in order to start app)
+onPause, on Stop, on Destroy (all must be called to stop app)
+onPause, on Stop, on Destroy (all must be called to stop app)
+make sure to SAVE whenever the user makes a change!
+in onStart - put all loading data
+
+ListView widget - used to show the android user things that are in our ArrayList
+ListView widget - uses arrayAdapter to get info from ArrayList to display on ListView
+ArrayLost - add, clear
+ListView - fonts, colours, clicking things
+ArrayAdapter - know when data changes
+*/
+
+
+
+
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileInputStream;  
+import java.io.FileNotFoundException;  
+import java.io.FileOutputStream;  
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -23,24 +40,28 @@ public class LonelyTwitterActivity extends Activity {
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
+	private ArrayList<String> tweets;//attribute, somehow needed to update things instantly (also need to become array list to change size of array)
+	private ArrayAdapter<String> adapter;
 	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.main); //runs lines from main.xml (in layout folder)
 
-		bodyText = (EditText) findViewById(R.id.body);
-		Button saveButton = (Button) findViewById(R.id.save);
+		bodyText = (EditText) findViewById(R.id.body);  //contacts body widget
+		Button saveButton = (Button) findViewById(R.id.save);  //id.save contacts button widget?
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
 
-			public void onClick(View v) {
+			public void onClick(View v) { //runs when click SAVE button
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
-				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
+				tweets.add(text);
+				adapter.notifyDataSetChanged();// added to allow change of info
+				saveInFile(text, new Date(System.currentTimeMillis()));//right now, stuff is saved to file, but tweets are not displyed 'till app is opened again.
+				//finish();//used to close an activity,  got rid of this to stop app from closing on button press
 
 			}
 		});
@@ -50,13 +71,15 @@ public class LonelyTwitterActivity extends Activity {
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		tweets = loadFromFile();  //doesn't need type since is attribute
+		
+		//Create a new ArrayAdapter and connect it to both the array and the widget
+		adapter = new ArrayAdapter<String>(this,
 				R.layout.list_item, tweets);
 		oldTweetsList.setAdapter(adapter);
 	}
 
-	private String[] loadFromFile() {
+	private ArrayList<String> loadFromFile() {//now same type as declaired above
 		ArrayList<String> tweets = new ArrayList<String>();
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
@@ -74,7 +97,7 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return tweets.toArray(new String[tweets.size()]);
+		return tweets;
 	}
 	
 	private void saveInFile(String text, Date date) {
